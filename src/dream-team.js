fefc42833ad58e92e14997e33dcbe4f10d8c3621
Marina-Poprotskaya <60131955+Marina-Poprotskaya@users.noreply.github.com>
2020-02-22 15:36:49 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,21 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+       return false;
+      }
+  function getString(value) {
+    return typeof(value) === 'string';
+  }
+  let newMembers = members.filter(getString);
+  
+  let string=newMembers.join(', ').replace(/\s/g, '').replace(/(,)/g, ' ');
+  string = string.toUpperCase();
+  function firstChar(value, index, arr) {  
+    if (index == 0)  
+        return true;  
+    else  
+        return arr[index - 1] === " ";  
+  }  
+  let array = [].filter.call(string, firstChar).sort().join('');
+return array;
+
 };
